8d4eb6657d3523ddf648de8f372efa43a820e169
Mohamed Gamal <mohamedgamalfahmy1@gamil.com>
2025-08-17 21:41:17 +0300

    Initial commit - Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='container mx-0 max-w-full bg-amber-300   '>
+      <ul className=' flex  items-center justify-around'>
+        <li>home</li>
+        <li>page</li>
+        <li>stting</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
